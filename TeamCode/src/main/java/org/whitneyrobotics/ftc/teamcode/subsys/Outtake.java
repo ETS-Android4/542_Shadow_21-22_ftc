c6a129a179b9ace048aebe5c26ec6027eefcb9a8
katelyn mak <superkmak11@gmail.com>
2021-10-14 17:40:13 -0700

    Outtake linear slides toggler

--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/subsys/Outtake.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/subsys/Outtake.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.whitneyrobotics.ftc.teamcode.lib.util.SimpleTimer;
 import org.whitneyrobotics.ftc.teamcode.lib.util.Toggler;
 
 public class Outtake {
@@ -15,9 +16,58 @@ public class Outtake {
         gate = outtakeMap.servo.get("gateServo");
         linearSlides = outtakeMap.get(DcMotorEx.class, "linearSlides");
     }
-    private double level1 = 1;
-    private double level2 = 2;
-    private double level3 = 3;
+    private int level1 = 1;
+    private int level2 = 2;
+    private int level3 = 3;
     private Toggler servoGateTog = new Toggler(2);
     private Toggler linearSlidesTog = new Toggler (3);
+    private SimpleTimer outtakeTimer = new SimpleTimer();
+
+    public boolean slidingInProgress = false;
+    private boolean outtakeTimerSet = true;
+
+    //toggler based teleop
+    public void togglerOuttake(boolean up,boolean down){
+        linearSlidesTog.changeState(up,down);
+        if (linearSlidesTog.currentState() == 0) {
+            if(linearSlides.getCurrentPosition()>level1){
+                linearSlides.setPower(-1);
+            } else if (linearSlides.getCurrentPosition()<level1){
+                linearSlides.setPower(1);
+            } else {
+                linearSlides.setPower(0);
+            }
+            slidingInProgress = true;
+            if (linearSlides.getCurrentPosition() == level1) {
+                slidingInProgress = false;
+            }
+        } else if (linearSlidesTog.currentState() == 1) {
+            if(linearSlides.getCurrentPosition()>level2){
+                linearSlides.setPower(-1);
+            } else if (linearSlides.getCurrentPosition()<level2){
+                linearSlides.setPower(1);
+            } else {
+                linearSlides.setPower(0);
+            }
+            slidingInProgress = true;
+            if (linearSlides.getCurrentPosition() == level2) {
+                slidingInProgress = false;
+            }
+        } else if (linearSlidesTog.currentState() == 2) {
+            if(linearSlides.getCurrentPosition()>level3){
+                linearSlides.setPower(-1);
+            } else if (linearSlides.getCurrentPosition()<level3){
+                linearSlides.setPower(1);
+            } else {
+                linearSlides.setPower(0);
+            }
+            slidingInProgress = true;
+            if (linearSlides.getCurrentPosition() == level3) {
+                slidingInProgress = false;
+            }
+        }
+    }
+    public void togglerServoGate(boolean open){
+
+    }
 }
