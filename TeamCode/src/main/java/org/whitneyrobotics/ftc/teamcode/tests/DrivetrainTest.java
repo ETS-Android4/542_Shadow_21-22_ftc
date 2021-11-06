fcef469469a23e59a9bcb3b9c62b9f51b81d0551
Perry <perryhan0123@gmail.com>
2021-11-06 16:53:29 -0700

    DrivetrainTest

--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/tests/DrivetrainTest.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/tests/DrivetrainTest.java
@@ -4,59 +4,41 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.whitneyrobotics.ftc.teamcode.autoop.AutoSwervePositions;
+import org.whitneyrobotics.ftc.teamcode.lib.geometry.Coordinate;
 import org.whitneyrobotics.ftc.teamcode.lib.geometry.Position;
 import org.whitneyrobotics.ftc.teamcode.lib.util.Toggler;
-import org.whitneyrobotics.ftc.teamcode.subsys.Drivetrain;
 import org.whitneyrobotics.ftc.teamcode.subsys.WHSRobotImpl;
 
 @TeleOp(name="Drivetrain Test" , group="Tests" )
 public class DrivetrainTest extends OpMode  {
-    public WHSRobotImpl yeetboi;
+    public WHSRobotImpl robot;
     // yeetboi is robot
     public Toggler posTog;
+    public Toggler topBottomTog;
     public Toggler allianceTog;
     public int posStateNum;
-    public int allianceStateNum;
+    public int allianceNum;
+    public int topOrBottom;
+
+    private Position[][] startingPositions = new Position[2][2];
+    private Position[] shippingHubPosition = new Position[2];
+    private Position[] sharedShippingHub = new Position[2];
+    private Position[] warehouse = new Position[2];
+    private Position[][] finalParkingPosition = new Position[2][2];
+    private Position[] carouselPositions = new Position[2];
+
+    private int RED = 0;
+    private int BLUE = 1;
+    private int BOTTOM = 0;
+    private int TOP = 1;
+
     @Override
     public void init() {
-        yeetboi = new WHSRobotImpl(hardwareMap);
+        robot = new WHSRobotImpl(hardwareMap);
         posTog = new Toggler(5);
         allianceTog = new Toggler(2);
-    }
+        topBottomTog = new Toggler(2);
 
-    @Override
-    public void loop() {
-        posTog.changeState(gamepad1.dpad_right, gamepad1.dpad_left);
-        allianceTog.changeState(gamepad1.dpad_up, gamepad1.dpad_down);
-
-        posStateNum = posTog.currentState();
-        allianceStateNum = allianceTog.currentState();
-
-        switch (posStateNum){
-            case 0:
-                yeetboi.driveToTarget(AutoSwervePositions.startToCarousel2, false);
-                break;
-            case 1:
-                yeetboi.driveToTarget(AutoSwervePositions.carouselToShippingHub2, true);
-                break;
-            case 2:
-                yeetboi.driveToTarget(AutoSwervePositions.shippingHubToWarehouse2, false);
-                break;
-            case 3:
-                yeetboi.driveToTarget(AutoSwervePositions.warehouseToShippingHub2, true);
-                break;
-            case 4:
-                switch (allianceStateNum){
-                    case 0:
-                        yeetboi.driveToTarget(AutoSwervePositions.shippingHubToWarehousePark2, false);
-                        break;
-                    case 1:
-                        yeetboi.driveToTarget(AutoSwervePositions.shippingHubToStorageUnit2, false);
-                        break;
-                }
-                break;
-
-        }
         startingPositions[RED][BOTTOM] = new Position(-1647.6,900);
         startingPositions[RED][TOP] = new Position(-1647.6,-150);
         startingPositions[BLUE][BOTTOM] = new Position(-1647.6,-900);
@@ -78,8 +60,42 @@ public class DrivetrainTest extends OpMode  {
 
         carouselPositions[RED] = new Position(-1400,1400);
         carouselPositions[BLUE] = new Position(-1400, -1400);
+    }
+
+    @Override
+    public void loop() {
+        posTog.changeState(gamepad1.dpad_right, gamepad1.dpad_left);
+        allianceTog.changeState(gamepad1.a);
+        topBottomTog.changeState(gamepad1.dpad_up, gamepad1.dpad_down);
+
+        posStateNum = posTog.currentState();
+        allianceNum = allianceTog.currentState();
+        topOrBottom = topBottomTog.currentState();
+
+        switch (posStateNum){
+            case 0:
+                if(gamepad1.x){
+                    Coordinate initial = new Coordinate(startingPositions[allianceTog.currentState()][topBottomTog.currentState()],0);
+                    robot.setInitialCoordinate(initial);
+                }
+                break;
+            case 1:
+                robot.driveToTarget(carouselPositions[allianceNum], true);
+                break;
+            case 2:
+                robot.driveToTarget(shippingHubPosition[allianceNum], true);
+                break;
+            case 3:
+                robot.driveToTarget(warehouse[allianceNum], false);
+                break;
+            case 4:
+                robot.driveToTarget(finalParkingPosition[allianceNum][topOrBottom],false);
+                break;
+
+        }
         
         telemetry.addData("Position State: ", posStateNum);
-        telemetry.addData("Alliance State: ", allianceTog);
+        telemetry.addData("Alliance State: ", allianceNum);
+        telemetry.addData("Top or Bottom",topOrBottom);
     }
 }
